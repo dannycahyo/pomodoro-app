d31a6c0758da56eea39d4ff84fea3e6e454fe708
dannycahyo <dannydwicahyono@gmail.com>
2023-03-12 16:44:13 +0700

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import App from "./App";
+import "./index.css";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        backgroundColor: "#091227",
+      },
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
