bae5bab54201f19e5ac7074e58ddd455f63202ee
dannycahyo <dannydwicahyono@gmail.com>
2023-03-13 17:06:05 +0700

    implement task state management

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Box,
   Container,
@@ -10,10 +11,30 @@ import {
   CircularProgress,
   CircularProgressLabel,
   IconButton,
+  Input,
 } from "@chakra-ui/react";
 import { BsPencil, BsPlay } from "react-icons/bs";
 
 const Home = () => {
+  const [task, setTask] = useState({
+    isEditing: false,
+    value: "",
+  });
+
+  const handleTaskChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTask((task) => ({
+      ...task,
+      value: e.target.value,
+    }));
+  };
+
+  const handleEditTask = () => {
+    setTask((task) => ({
+      ...task,
+      isEditing: !task.isEditing,
+    }));
+  };
+
   return (
     <Container centerContent minH="70vh">
       <Center>
@@ -21,10 +42,33 @@ const Home = () => {
           <Heading size="lg" color="#83A5F7" pb="6">
             Pomodoro Timer
           </Heading>
-          <Tag size="lg" colorScheme="blue" borderRadius="full" py="2" px="4">
-            <TagLabel>Task: Write an article</TagLabel>
-            <TagRightIcon as={BsPencil} />
-          </Tag>
+          {task.isEditing ? (
+            <Input
+              color="white"
+              value={task.value}
+              onChange={handleTaskChange}
+              onBlur={handleEditTask}
+            />
+          ) : (
+            <>
+              <Tag
+                size="lg"
+                colorScheme="blue"
+                borderRadius="full"
+                py="2"
+                px="4"
+              >
+                <TagLabel>
+                  {task.value === "" ? "What's your task!" : task.value}
+                </TagLabel>
+                <TagRightIcon
+                  _hover={{ cursor: "pointer" }}
+                  as={BsPencil}
+                  onClick={handleEditTask}
+                />
+              </Tag>
+            </>
+          )}
         </VStack>
       </Center>
 
